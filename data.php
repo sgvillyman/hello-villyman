76a30f3c97b8ee575e8615814f4a94f7f8651f24
sgvillyman <43415747+sgvillyman@users.noreply.github.com>
2020-01-27 16:47:45 +0300

    update data
    add comment on data.php code

--- data.php
+++ data.php
@@ -1,12 +1,13 @@
 <?php
+//json header
 header('Content-Type: application/json');
-
+//connection string
 $conn = mysqli_connect("localhost","root","","help_db");
-
+//sql query
 $sqlQuery = "SELECT student_id,student_name,marks FROM tbl_marks ORDER BY student_id";
 
 $result = mysqli_query($conn,$sqlQuery);
-
+//fetch from database
 $data = array();
 foreach ($result as $row) {
 	$data[] = $row;
